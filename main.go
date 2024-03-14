2f3e77ff3aed5b89047123198e569eb7edff3a92
lasse <77295879+lassejlv@users.noreply.github.com>
2024-03-14 22:38:43 +0100

    Api key protection

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"os"
 
 	"log"
@@ -9,6 +13,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var API_URL string = "https://api.chucknorris.io/jokes/random"
@@ -23,7 +30,7 @@ type Joke struct {
 }
 
 type API_KEY struct {
-	Key string
+	Key string `bson:"key"`
 }
 
 func main() {
@@ -33,9 +40,47 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	// Database connection
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(os.Getenv("DATABASE_URL")))
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("⛺️ Database has connected!")
+	}
+	defer client.Disconnect(context.Background())
+
+	// Database insert schema
+	// Accessing a database and collection
+	collection := client.Database("go-joke-api").Collection("api_keys")
+
 	r := gin.Default()
 
 	r.GET("/joke", func(c *gin.Context) {
+
+		apikey := c.Query("api-key")
+
+		if apikey == "" {
+			c.JSON(http.StatusForbidden, gin.H{
+				"message": "Missing api_key as a query",
+			})
+			return
+		}
+
+		// Define a filter to search for the API key
+		filter := bson.M{"key": apikey}
+
+		// Find key in the database
+		var result API_KEY
+		err = collection.FindOne(context.Background(), filter).Decode(&result)
+
+		if err == mongo.ErrNoDocuments {
+			c.JSON(http.StatusForbidden, gin.H{
+				"message": "Invalid api key",
+			})
+			return
+		}
+
 		joke := GetJoke()
 
 		c.JSON(http.StatusOK, gin.H{
@@ -43,6 +88,20 @@ func main() {
 		})
 	})
 
+	r.POST("/api-key", func(c *gin.Context) {
+		apiKey := API_KEY{Key: CreateCode()}
+		_, err = collection.InsertOne(context.Background(), apiKey)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "Could not create an api key :/",
+			})
+		} else {
+			c.JSON(http.StatusCreated, gin.H{
+				"data": apiKey,
+			})
+		}
+	})
 	// Listen and serve on 0.0.0.0:8080
 	port := ":" + os.Getenv("PORT")
 	r.Run(port)
@@ -65,4 +124,25 @@ func GetJoke() Joke {
 	}
 
 	return joke
+}
+
+func CreateCode() string {
+	// Define the length of the API key
+	keyLength := 32 // You can adjust the length as needed
+
+	// Create a byte slice to store random bytes
+	randomBytes := make([]byte, keyLength)
+
+	// Fill the slice with random bytes
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		// Handle error, if any
+		panic(err)
+	}
+
+	// Encode the random bytes to a hexadecimal string
+	key := hex.EncodeToString(randomBytes)
+
+	// Return the API key
+	return key
 }
